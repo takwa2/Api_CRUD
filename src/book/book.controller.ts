4fa9673b999856e2d31f8417357b698629572bbb
Takwa Amara <takwaamara92@gmail.com>
2024-04-21 14:50:25 +0100

    first commit

---
+++ src/book/book.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Book } from './book.model';
+import { BookService } from './book.service';
+import { get } from 'http';
+
+@Controller('API_CRUD/v1/book')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Get()
+  async getAllBook(): Promise<Book[]> {
+    return this.bookService.getAllBook();
+  }
+
+  @Post()
+  async postBook(@Body() postData: Book): Promise<Book> {
+    return this.bookService.createBook(postData);
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') id: number): Promise<Book | null> {
+    return this.bookService.getBook(id);
+  }
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: number): Promise<Book> {
+    return this.bookService.deleteBook(id);
+  }
+
+  @Put(':id')
+  async updateBook(
+    @Param('id') id: number,
+    @Body() postData: Book,
+  ): Promise<Book> {
+    return this.bookService.updateBook(id, postData);
+  }
+}
